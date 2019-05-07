6449a2d5c2269c53a254bcbfe8f4efd8c3f9fa8a
jBalasis <cs2180008@di.uoa.gr>
2019-05-07 18:00:19 +0300

    this actually seems plausible

--- projects/EOSC/src/app/app.component.ts
+++ projects/EOSC/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {BaseAppComponent} from '../../../../src/app/base-app.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends BaseAppComponent{
   title = 'EOSC';
 }
--- projects/EOSC/src/app/app.module.ts
+++ projects/EOSC/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BaseAppModule} from '../../../../src/app/base-app.module';
+import {FooterComponent} from './shared/footer/footer.component';
+import {BreadcrumbsComponent} from './shared/breadcrumbs/breadcrumbs.component';
+import {TopMenuComponent} from './shared/topmenu/topmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // PERSISTENT
+    FooterComponent,
+    BreadcrumbsComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +22,9 @@ import {BaseAppModule} from '../../../../src/app/base-app.module';
     BaseAppModule
   ],
   providers: [],
+  exports: [
+    TopMenuComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
